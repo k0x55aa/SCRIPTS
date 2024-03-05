24b401884a4e9b18b95dac34f050ddd1bfc6959a
k0x55aa <143346776+k0x55aa@users.noreply.github.com>
2024-03-06 00:07:15 +0530

    Update regSearch.ps1

--- regSearch.ps1
+++ regSearch.ps1
@@ -4,3 +4,4 @@ $searchString = "Strings to search"
 $regvalFound = Get-ChildItem -Path Registry::HKEY_CLASSES_ROOT -Recurse |
     Where-Object { $_.PSIsContainer -and $_.PSPath -match $searchString } |
     Select-Object -ExpandProperty PSPath
+echo $regvalFound
